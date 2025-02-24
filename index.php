7f7f5fbc8b96debcd49cfcb70613c0d5f17b7185
dafezzz <daveizalibama9@gmail.com>
2025-02-23 13:49:38 -1200

    Add files via upload

--- index.php
+++ index.php
@@ -1,1 +1,9 @@
 ini tampilanya
+
+<li>
+						<a href="logout.php" onclick="return confirm('Anda yakin keluar dari aplikasi ?')">
+							<i class="fa fa-sign-out"></i>
+							<span>Logout</span>
+							<span class="pull-right-container"></span>
+						</a>
+					</li>
